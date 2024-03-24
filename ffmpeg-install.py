6bf9647221ff39782351d29155b302878e150a0e
ChuishenX <aaaa@oblivionocean.top>
2024-03-24 14:32:27 +0800

    Update ffmpeg-install.py

--- ffmpeg-install.py
+++ ffmpeg-install.py
@@ -1,15 +1,17 @@
 import platform
 import os
 import requests
+import py7zr
+import shutil
 
 pm = platform.machine()
 
 def linuxInstall():
     name = f"ffmpeg-{pm}.tar.xz"
     if pm.capitalize() in ("AMD64","ARM64","I686","ARMHF","ARMEL"):
-        file = requests.get(f"https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-{pm}-static.tar.xz").content
+        con = requests.get(f"https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-{pm}-static.tar.xz").content
         with open(name, "wb") as f:
-            f.write(file)
+            f.write(con)
             os.system(f"tar xvJf {name}")
             print("We have downloaded the suitable version of FFMpeg for you in the same folder. Please check it out")
         for root, dirs, files in os.walk("."):
@@ -35,13 +37,32 @@ if pf == 'Darwin':
     except:
         print("Something's WRONG?")
         print("Wow, you should check your brew, if you haven't install brew, install one in (https://brew.sh/).")
+
 elif pf == 'Linux':
     try:
         print("System: Linux")
         linuxInstall()
     except:
         print("A problem occured when running this program. Please try to install ffmpeg manually.")
+
 elif pf == 'Windows':
-    pass
+    print("Please RUN AS AN ADMIN.")
+    try:
+        spath = os.path.expandvars("%windir%\\System32")
+        cont = requests.get("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.7z")
+        with open('ffmpeg-essencials.7z', "wb") as f:
+            f.write(cont)
+        with py7zr.SevenZipFile('ffmpeg-essentials.7z', mode='r') as z:
+            z.extractall()
+        for root, dirs, files in os.walk("."):
+                if "ffmpeg" in dirs:
+                    os.chdir(dirs)
+        os.chdir("bin")
+        shutil.copy("ffmpeg.exe",spath+"\\ffmpeg.exe")
+        shutil.copy("ffplay.exe",spath+"\\ffplay.exe")
+        shutil.copy("ffprobe.exe",spath+"\\ffprobe.exe")
+        print("Successfully installed FFMpeg to your PC.")
+    except:
+        print("A problem occured when running this program. Please try to install ffmpeg manually.")
 
 input("**Press [Enter] to EXIT!**")
